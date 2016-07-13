e724145f7c8490fffb09bfe268c15477307f371c
eryx67 <eryx67@gmail.com>
2016-07-13 12:27:16 +0500

    lift some functions

--- src/Network/Libtorrent/Sha1Hash.hs
+++ src/Network/Libtorrent/Sha1Hash.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE FlexibleInstances #-}
--- | 
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+-- |
 
 module Network.Libtorrent.Sha1Hash (Sha1Hash
                            , unSha1Hash
@@ -11,20 +11,21 @@ module Network.Libtorrent.Sha1Hash (Sha1Hash
                            , sha1HashToByteString
                            ) where
 
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import           Foreign.ForeignPtr ( ForeignPtr, withForeignPtr )
-import           Foreign.Marshal.Array (allocaArray)
-import qualified Language.C.Inline as C
-import qualified Language.C.Inline.Cpp as C
-import qualified Language.C.Inline.Unsafe as CU
-import           System.IO.Unsafe (unsafePerformIO)
+import           Control.Monad.IO.Class      (MonadIO, liftIO)
+import           Data.ByteString             (ByteString)
+import qualified Data.ByteString             as BS
+import           Foreign.ForeignPtr          (ForeignPtr, withForeignPtr)
+import           Foreign.Marshal.Array       (allocaArray)
+import qualified Language.C.Inline           as C
+import qualified Language.C.Inline.Cpp       as C
+import qualified Language.C.Inline.Unsafe    as CU
+import           System.IO.Unsafe            (unsafePerformIO)
 
 
 import           Network.Libtorrent.Inline
 import           Network.Libtorrent.Internal
-import           Network.Libtorrent.Types
 import           Network.Libtorrent.String
+import           Network.Libtorrent.Types
 
 
 C.context libtorrentCtx
@@ -56,17 +57,17 @@ instance WithPtr Sha1Hash where
 sha1HashSize :: Int
 sha1HashSize = 20
 
--- | Create 'Sha1Hash' from 'ByteString'. Bytestring must be 20 byte length. 
-newSha1Hash :: ByteString -> IO (Maybe Sha1Hash)
+-- | Create 'Sha1Hash' from 'ByteString'. Bytestring must be 20 byte length.
+newSha1Hash :: MonadIO m =>  ByteString -> m (Maybe Sha1Hash)
 newSha1Hash bs
   | BS.length bs /= sha1HashSize =
     return Nothing
   | otherwise =
-    Just <$> fromPtr [CU.exp| sha1_hash * { new sha1_hash($bs-ptr:bs) }|]
+    liftIO $ Just <$> fromPtr [CU.exp| sha1_hash * { new sha1_hash($bs-ptr:bs) }|]
 
-sha1HashToByteString :: Sha1Hash -> IO ByteString
+sha1HashToByteString :: MonadIO m => Sha1Hash -> m ByteString
 sha1HashToByteString sh =
-  withPtr sh $ \shPtr -> do
+  liftIO . withPtr sh $ \shPtr -> do
   let shSize = fromIntegral sha1HashSize
   allocaArray sha1HashSize $ \cstr -> do
     [CU.block| void {
